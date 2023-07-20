0034fca9e72cd4a072265a3977f1735f29bee7e4
Gama <loviedo.1987@gmail.com>
2023-07-19 23:45:33 -0600

    control de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
+import { generarId } from './Helpers/Index'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const [gastos, setGastos] = useState([])
+
+  const handleNuevoGasto = () =>{
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id = generarId();
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)
+    }, 500);
+  }
   return (
     <div>
       <Header 
@@ -13,6 +38,21 @@ function App() {
         isValidPresupuesto = {isValidPresupuesto}
         setIsValidPresupuesto = {setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img 
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+      {modal && <Modal 
+                    setModal = {setModal}
+                    animarModal = {animarModal}
+                    setAnimarModal = {setAnimarModal}
+                    guardarGasto={guardarGasto}
+                />}
     </div>
   )
 }
