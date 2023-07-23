6c1b1005253b46db0b6b5c456ea0a09ac8118c62
Gama <loviedo.1987@gmail.com>
2023-07-22 23:51:49 -0600

    react-swipeable-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import { object } from 'prop-types'
 
 function App() {
   const [gastos, setGastos] = useState([])
@@ -14,7 +15,18 @@ function App() {
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
 
-  
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  useEffect(() => {
+    if( Object.keys(gastoEditar).length > 0 ) {
+        setModal(true)
+
+        setTimeout(() => {
+            setAnimarModal(true)
+        }, 500);
+    }
+  }, [ gastoEditar ])
+
 
   const handleNuevoGasto = () =>{
     setModal(true)
@@ -48,6 +60,7 @@ function App() {
           <main>
             <ListadoGastos 
               gastos={gastos}
+              setGastoEditar={setGastoEditar}
             />
           </main>
           <div className='nuevo-gasto'>
