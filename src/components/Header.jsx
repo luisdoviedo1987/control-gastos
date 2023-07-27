668c430ece4240518a44acaff922ec8bfc3da32f
Gama <loviedo.1987@gmail.com>
2023-07-27 14:11:45 -0600

    resetear app y fin de proyecto
    se resetea la app y con esto se finaliza el proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 const Header = ({
   gastos,
+  setGastos,
   presupuesto, 
   setPresupuesto, 
   isValidPresupuesto, 
@@ -14,7 +15,10 @@ const Header = ({
         {isValidPresupuesto ? (
           <ControlPresupuesto 
             gastos = {gastos}
+            setGastos = {setGastos}
             presupuesto = {presupuesto}
+            setPresupuesto = {setPresupuesto}
+            setIsValidPresupuesto = {setIsValidPresupuesto}
           />
         ) : (
           <NuevoPresupuesto 
